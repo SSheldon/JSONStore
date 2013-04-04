f3afd77b0e0cee30058500dc7dc45ea50191c080
Steven Sheldon <steven@sasheldon.com>
2013-04-04 13:15:26 -0500

    Added empty JSONStore class files.

---
+++ JSONStore/JSONStore.h
@@ -0,0 +1,13 @@
+//
+//  JSONStore.h
+//
+//  Created by Steven Sheldon on 4/4/13.
+//  Copyright (c) 2013 Steven Sheldon. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <CoreData/CoreData.h>
+
+@interface JSONStore : NSIncrementalStore
+
+@end
---
+++ JSONStore/JSONStore.m
@@ -0,0 +1,12 @@
+//
+//  JSONStore.m
+//
+//  Created by Steven Sheldon on 4/4/13.
+//  Copyright (c) 2013 Steven Sheldon. All rights reserved.
+//
+
+#import "JSONStore.h"
+
+@implementation JSONStore
+
+@end
