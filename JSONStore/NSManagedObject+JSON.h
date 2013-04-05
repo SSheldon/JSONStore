f7c31001821887bb1d07e05a6a71b21d212bf059
Steven Sheldon <steven@sasheldon.com>
2013-04-05 18:31:24 -0500

    Added NSManagedObject JSON category.

---
+++ JSONStore/NSManagedObject+JSON.h
@@ -0,0 +1,16 @@
+//
+//  NSManagedObject+JSON.h
+//
+//  Created by Steven Sheldon on 4/3/13.
+//  Copyright (c) 2013 Steven Sheldon. All rights reserved.
+//
+
+#import <CoreData/CoreData.h>
+
+@interface NSManagedObject (JSON)
+
+- (NSDictionary *)JSONDictionary;
+- (NSData *)JSONDataPrettyPrinted:(BOOL)prettyPrinted;
+- (NSString *)JSONStringPrettyPrinted:(BOOL)prettyPrinted;
+
+@end
